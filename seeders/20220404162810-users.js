aca7e437f7f65be5894c29914e8eb2e18603c712
chloeWieser <chloerosewieser@gmail.com>
2022-04-04 15:11:11 -0400

    blgs

---
+++ seeders/20220404162810-users.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }, {
+     *    name: 'John Doe',
+     *   isBetaMember: false}
+     * ], {});
+    */
+
+     await queryInterface.bulkInsert('users', [{
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'jd@email.com',
+      isPublished: false
+    }, 
+    {
+      firstName: 'Mary',
+      lastName: 'Williams',
+      email: 'mw@email.com',
+      isPublished: true
+    }
+    ], {});
+  },
+
+async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
+
+
